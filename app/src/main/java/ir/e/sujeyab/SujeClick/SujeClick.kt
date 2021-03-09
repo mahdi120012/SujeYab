b801508883e15a12b5670b1a2b5558a82604e9a6
mahdi120012 <mahdi120012@gmail.com>
2021-03-09 14:02:24 +0330

    update 99.12.19

--- app/src/main/java/ir/e/sujeyab/SujeClick/SujeClick.kt
+++ app/src/main/java/ir/e/sujeyab/SujeClick/SujeClick.kt
@@ -1,9 +1,13 @@
 package ir.e.sujeyab.SujeClick
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
 import ir.e.sujeyab.R
+import jp.wasabeef.picasso.transformations.CropCircleTransformation
 import kotlinx.android.synthetic.main.suje_click.*
+import kotlinx.android.synthetic.main.suje_click.txOnvan
 
 class SujeClick : AppCompatActivity() {
 
@@ -17,7 +21,40 @@ class SujeClick : AppCompatActivity() {
         var picture:String = intent.extras!!.getString("picture").toString()
         var motavali:String = intent.extras!!.getString("motavali").toString()
         var modat_baghi_mande:String = intent.extras!!.getString("modat_baghi_mande").toString()
+        var axFerestande:String = intent.extras!!.getString("axFerestande").toString()
 
         txOnvan.setText(onvan)
+        txMatn.setText(matn)
+        txMatnKamel.setText(matn)
+
+        loadImage(imgPicture,picture)
+        loadImage(imgAxFerestande,axFerestande)
+
+    }
+
+
+    fun loadImage(img:ImageView, pictureLink:String){
+
+        //line zir baraye circle kardane imageView ee
+        //.transform(CropCircleTransformation())
+
+        if (pictureLink.isEmpty()) {
+            Picasso.get()
+                    .load(R.drawable.logo)
+                    .centerInside()
+                    .fit()
+                    .error(R.drawable.logo)
+                    .placeholder(R.drawable.logo)
+                    .into(img)
+        } else {
+            Picasso.get()
+                    .load(pictureLink)
+                    .centerInside()
+                    .fit()
+                    .error(R.drawable.logo)
+                    .placeholder(R.drawable.logo)
+                    .into(img)
+        }
+
     }
 }
